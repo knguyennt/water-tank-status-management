fb4004ca55549a1e5724c59cc34067dac51aca56
knguyennt <khoanguyennt2010@gmail.com>
2024-05-29 23:43:04 +0700

    chore: update cors server

--- api/app.js
+++ api/app.js
@@ -12,12 +12,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors())
 
  const corsOptions = {
    origin: 'http://localhost:3000/',
    optionsSuccessStatus: 200,
  };
+
+ app.use(cors(corsOptions))
 
  app.use(cors(corsOptions));
 
